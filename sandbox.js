f956847d06b88251ae0e19fb90efc59448181fd1
MakarandPundlik <mmp2308@gmail.com>
2021-04-05 14:57:02 +0530

    first commit

---
+++ sandbox.js
@@ -0,0 +1,7 @@
+var ch = 'mychar';
+console.log(ch);
+var inputs = document.querySelectorAll('input');
+//console.log(inputs);
+inputs.forEach(function (input) {
+    console.log(input);
+});
---
+++ sandbox.ts
@@ -0,0 +1,11 @@
+const ch = 'mychar';
+
+console.log(ch);
+
+const inputs = document.querySelectorAll('input');
+
+//console.log(inputs);
+
+inputs.forEach((input)=>{
+    console.log(input);
+})
